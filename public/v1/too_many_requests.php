d90b2c6662b600b70f0e27a30c0e4932c232c83a
ffiaux <ffiaux@gmail.com>
2019-07-16 20:04:37 -0300

    Too many requests file

---
+++ public/v1/too_many_requests.php
@@ -0,0 +1,10 @@
+<html>
+  <head>
+	<title>Too Many Requests</title>
+  </head>
+  <body>
+	<h1>Too Many Requests</h1>
+	<p>I only allow 50 requests per hour to this Web site per
+	logged in user. Try again soon.</p>
+  </body>
+</html>
